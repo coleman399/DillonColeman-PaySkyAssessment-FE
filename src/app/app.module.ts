388d10ead77875194b08564323e48cafaa0293cd
dillon.coleman <coleman399@gmail.com>
2024-01-13 19:03:48 -0600

    more restructuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { VacanciesComponent } from './vacancies/vacancies.component';
-import { CreateNewVacancyFormComponent } from './vacancies/create-new-vacancy-form/create-new-vacancy-form.component';
-import { CreateNewAccountFormComponent } from './login/create-new-account-form/create-new-account-form.component';
-import { LoginFormComponent } from './login/login-form/login-form.component';
+import { HomeComponent } from './header/home/home.component';
+import { LoginComponent } from './header/login/login.component';
+import { VacanciesComponent } from './header/vacancies/vacancies.component';
+import { CreateNewVacancyFormComponent } from './header/vacancies/create-new-vacancy-form/create-new-vacancy-form.component';
+import { CreateNewAccountFormComponent } from './header/login/create-new-account-form/create-new-account-form.component';
+import { LoginFormComponent } from './header/login/login-form/login-form.component';
 
 @NgModule({
   declarations: [
